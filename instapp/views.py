19cf8975a8d9b203d3382180a19b156d81bac729
bensammwaniki <bensammwaniki@gmail.com>
2021-10-18 12:42:11 +0300

    migrations

--- instapp/views.py
+++ instapp/views.py
@@ -8,13 +8,13 @@ import cloudinary.api
 
 @login_required(login_url='/accounts/login/')
 def home(request):
-    images = Image.objects.all().order_by('-image_date')
+    images = Post.objects.all().order_by('-image_date')
     return render(request, 'index.html', {'images': images})
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
     current_user = request.user
-    images = Image.objects.filter(user_id=current_user.id)
+    images = Post.objects.filter(user_id=current_user.id)
     profile = Profile.objects.filter(user_id=current_user.id).first()
     return render(request, 'profile.html', {"images": images, "profile": profile})
 
