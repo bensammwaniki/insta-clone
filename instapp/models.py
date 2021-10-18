8d0ab86dea04fc6b421601be57a3c1bb5920bebe
bensammwaniki <bensammwaniki@gmail.com>
2021-10-18 09:50:38 +0300

    added profile model

--- instapp/models.py
+++ instapp/models.py
@@ -45,3 +45,30 @@ class Image(models.Model):
 
     def __str__(self):
         return self.image_name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_photo = CloudinaryField('image')
+
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    
+    contact = models.CharField(max_length=50, blank=True, null=True)
+
+    def update(self):
+        self.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_by_user(cls, user):
+        profile = cls.objects.filter(user=user)
+        return profile
+
+    def __str__(self):
+        return self.user.username
