f11ae22d1e0522d0f98240aa34bf4738fdc8d4bf
bensammwaniki <bensammwaniki@gmail.com>
2021-10-18 09:28:33 +0300

    added profile

--- instapp/views.py
+++ instapp/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 
 def home(request):
     return render(request,"index.html")
 
+@login_required(login_url='/accounts/login/')
 def profile(request):
-    return render(request,"profile.html") 
+    # current_user = request.user
+    # # get images for the current logged in user
+    # images = Image.objects.filter(user_id=current_user.id)
+    # # get the profile of the current logged in user
+    # profile = Profile.objects.filter(user_id=current_user.id).first()
+    return render(request, 'profile.html', {"images": images, "profile": profile})
 
 def login(request):
     return render(request,"login.html")
