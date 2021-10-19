1720aa04a2b4bdf5d3c5d2a751e6cc8e9eb3333e
bensammwaniki <bensammwaniki@gmail.com>
2021-10-19 07:43:37 +0300

    created test and run all test passed

--- instapp/tests.py
+++ instapp/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+# test image class
+
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        # create a user
+        user = User.objects.create(
+            username='test_user',
+            first_name='bensam',
+            last_name='mwaniki'
+        )
+        Post.objects.create(
+            image_name='test_image',
+            image='https://doographics.com/assets/dg/images/cooking-youtube-thumbnail/cooking',
+            image_caption='test image',
+            profile_id=user.id,
+            user_id=user.id
+        )
+
+    def test_image_name(self):
+        image = Post.objects.get(image_name='test_image')
+        self.assertEqual(image.image_name, 'test_image')
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username='test_user',
+            first_name='bensam',
+            last_name='mwaniki'
+        )
+        Profile.objects.create(
+            bio='test bio',
+            profile_photo='https://doographics.com/assets/dg/images/cooking-youtube-thumbnail/cooking',
+            user_id=user.id
+        )
+
+    def test_bio(self):
+        profile = Profile.objects.get(bio='test bio')
+        self.assertEqual(profile.bio, 'test bio')
